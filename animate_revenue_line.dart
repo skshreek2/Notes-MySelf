9ff075c67d6913fc19aebea1e6130ad4d63ef462
skshreek2 <103485010+skshreek2@users.noreply.github.com>
2026-03-10 22:45:17 +0530

    Update animate_revenue_line.dart

--- animate_revenue_line.dart
+++ animate_revenue_line.dart
@@ -1,19 +1,17 @@
 import 'dart:math' as math;
-
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:hdfc_merchant_app/core/util/common.dart';
+import 'package:hdfc_merchant_app/features/dashboard/model/chart_data.dart';
 
 class AnimatedRevenueLineChart extends StatefulWidget {
-  final List<FlSpot> spots;
-  final List<String> dateKeys;
+  final ChartData chartData;
   final double height;
   final String Function(int) getDateLabel;
 
   const AnimatedRevenueLineChart({
     super.key,
-    required this.spots,
-    required this.dateKeys,
+    required this.chartData,
     required this.height,
     required this.getDateLabel,
   });
@@ -25,34 +23,70 @@ class AnimatedRevenueLineChart extends StatefulWidget {
 
 class _AnimatedRevenueLineChartState extends State<AnimatedRevenueLineChart>
     with SingleTickerProviderStateMixin {
-  late AnimationController _revenueController;
-  late Animation<double> revenueAnimation;
+  late AnimationController controller;
+  late Animation<double> animation;
 
   @override
   void initState() {
     super.initState();
-    _revenueController = AnimationController(
-      duration: const Duration(milliseconds: 2400),
+
+    controller = AnimationController(
       vsync: this,
+      duration: const Duration(milliseconds: 3000),
     );
 
-    revenueAnimation = Tween(begin: 0.0, end: 1.0).animate(
-      CurvedAnimation(parent: _revenueController, curve: Curves.easeInOutBack),
+    animation = CurvedAnimation(
+      parent: controller,
+      curve: Curves.easeOutQuart,
     );
 
-    _revenueController.forward();
+    controller.forward();
   }
 
   @override
   void dispose() {
-    _revenueController.dispose();
+    controller.dispose();
     super.dispose();
   }
 
+   List<FlSpot> _buildAnimatedSpots(double progress) {
+  if (widget.spots.isEmpty) return [];
+
+  final spots = chartData.volumes;
+  final total = spots.length;
+
+  final value = progress * (total - 1);
+  final index = value.floor();
+  final remainder = value - index;
+
+  final animatedSpots = <FlSpot>[];
+
+  // add all completed spots
+  for (int i = 0; i <= index && i < total; i++) {
+    animatedSpots.add(spots[i]);
+  }
+
+  // interpolate next spot
+  if (index + 1 < total) {
+    final p1 = spots[index];
+    final p2 = spots[index + 1];
+
+    final x = p1.x + (p2.x - p1.x) * remainder;
+    final y = p1.y + (p2.y - p1.y) * remainder;
+
+    animatedSpots.add(FlSpot(x, y));
+  }
+
+  return animatedSpots;
+}
+
+
   @override
   Widget build(BuildContext context) {
+    final maxAmount = chartData.volumes.isNotEmpty
+        ? widget.spots.map((e) => e.y).reduce(math.max) * 1.1
+        : 500000.0;
 
-    final maxAmount = widget.spots.isNotEmpty ? widget.spots.map((e) => e.y).reduce(math.max) * 1.1 : 500000.0;
     return Container(
       height: widget.height,
       padding: const EdgeInsets.all(24),
@@ -61,122 +95,127 @@ class _AnimatedRevenueLineChartState extends State<AnimatedRevenueLineChart>
         borderRadius: BorderRadius.circular(16),
         border: Border.all(color: Theme.of(context).dividerColor),
       ),
-      child: Column(
-        children: [
-          SizedBox(height: 16),
-          Expanded(
-            child: TweenAnimationBuilder<double>(
-              tween: Tween(begin: 0.0, end: 1.0),
-              duration: const Duration(milliseconds: 1200),
-              curve: Curves.easeOutBack,
-              builder: (context, progress, child) {
-                final maxVisibleIndex = (widget.dateKeys.length * progress)
-                    .floor();
-                final visibleSpots = widget.spots
-                    .asMap()
-                    .entries
-                    .where((entry) => entry.key <= maxVisibleIndex)
-                    .map((entry) => FlSpot(entry.value.x, entry.value.y))
-                    .toList();
-                return LineChart(
-                  LineChartData(
-                    minX: 0,
-                    maxX: (widget.dateKeys.length - 1).toDouble(),
-                    maxY: maxAmount,
-                    minY: 0,
-                    gridData: FlGridData(
-                      show: true,
-                      drawVerticalLine: false,
-                      getDrawingHorizontalLine: (value) => FlLine(
-                        color: Theme.of(context).dividerColor,
-                        strokeWidth: 1,
-                      ),
-                    ),
-                    titlesData: FlTitlesData(
-                      bottomTitles: AxisTitles(
-                        axisNameWidget: Text("Date"),
-                        axisNameSize: 15,
-                        sideTitles: SideTitles(
-                          showTitles: true,
-                          reservedSize: 32,
-                          interval: 1,
-                          getTitlesWidget: (value, meta) {
-                            final index = value.toInt();
-                            if (index < 0 || index >= widget.dateKeys.length) {
-                              return SizedBox.shrink();
-                            }
-                            final day = widget.dateKeys[index].substring(8, 10);
-                            return Padding(
-                              padding: EdgeInsets.only(top: 8),
-                              child: Text(
-                                widget.getDateLabel(index),
-                                textAlign: TextAlign.center,
-                                style: TextStyle(
-                                  fontSize: 11,
-                                  fontWeight: FontWeight.w600,
-                                ),
-                              ),
-                            );
-                          },
-                        ),
-                      ),
-                      leftTitles: AxisTitles(
-                        axisNameWidget: Text("Amount (₹)"),
-                        axisNameSize: 15,
-                        sideTitles: SideTitles(
-                          showTitles: true,
-                          reservedSize: 60,
-                          interval: maxAmount / 5,
-                          getTitlesWidget: (value, meta) => Padding(
-                            padding: EdgeInsets.only(right: 8),
-                            child: Text(
-                              '  ${formatVolume(value)}',
-                              style: TextStyle(
-                                fontSize: 11,
-                                color: Theme.of(
-                                  context,
-                                ).textTheme.bodySmall?.color,
-                              ),
-                            ),
+      child: AnimatedBuilder(
+        animation: animation,
+        builder: (context, child) {
+          final animatedSpots = _buildAnimatedSpots(animation.value);
+
+          return LineChart(
+            LineChartData(
+              minX: 0,
+              maxX: (widget.dateKeys.length - 1).toDouble(),
+              minY: 0,
+              maxY: maxAmount,
+
+              gridData: FlGridData(
+                show: true,
+                drawVerticalLine: false,
+                getDrawingHorizontalLine: (value) => FlLine(
+                  color: Theme.of(context).dividerColor,
+                  strokeWidth: 1,
+                ),
+              ),
+
+              titlesData: FlTitlesData(
+                bottomTitles: AxisTitles(
+                  axisNameWidget: const Text("Date"),
+                  axisNameSize: 30,
+                  sideTitles: SideTitles(
+                    showTitles: true,
+                    interval: 1,
+                    reservedSize: 32,
+                    getTitlesWidget: (value, meta) {
+                      final index = value.toInt();
+
+                      if (index < 0 || index >= widget.dateKeys.length) {
+                        return const SizedBox.shrink();
+                      }
+
+                      return Padding(
+                        padding: const EdgeInsets.only(top: 8),
+                        child: Text(
+                          widget.getDateLabel(index),
+                          style: const TextStyle(
+                            fontSize: 11,
+                            fontWeight: FontWeight.w600,
                           ),
                         ),
-                      ),
-                      rightTitles: AxisTitles(
-                        sideTitles: SideTitles(showTitles: false),
-                      ),
-                      topTitles: AxisTitles(
-                        sideTitles: SideTitles(showTitles: false),
-                      ),
-                    ),
-                    borderData: FlBorderData(show: false),
-                    lineBarsData: [
-                      LineChartBarData(
-                        spots: visibleSpots,
-                        isCurved: true,
-                        curveSmoothness: 0.3,
-                        preventCurveOverShooting: true,
-                        gradient: LinearGradient(
-                          colors: [Colors.blue.shade600, Colors.blue.shade400],
-                        ),
-                        barWidth: 3,
-                        dotData: FlDotData(show: true),
-                        belowBarData: BarAreaData(
-                          show: true,
-                          gradient: LinearGradient(
-                            colors: [
-                              Colors.blue.withOpacity(0.3),
-                              Colors.transparent,
-                            ],
+                      );
+                    },
+                  ),
+                ),
+
+                leftTitles: AxisTitles(
+                  axisNameWidget: const Text("Amount (₹)"),
+                  axisNameSize: 30,
+                  sideTitles: SideTitles(
+                    showTitles: true,
+                    reservedSize: 60,
+                    interval: maxAmount / 5,
+                    getTitlesWidget: (value, meta) {
+                      return Padding(
+                        padding: const EdgeInsets.only(right: 8),
+                        child: Text(
+                          formatVolume(value),
+                          style: TextStyle(
+                            fontSize: 11,
+                            color: Theme.of(context)
+                                .textTheme
+                                .bodySmall
+                                ?.color,
                           ),
                         ),
-                      ),
+                      );
+                    },
+                  ),
+                ),
+
+                rightTitles: const AxisTitles(
+                  sideTitles: SideTitles(showTitles: false),
+                ),
+
+                topTitles: const AxisTitles(
+                  sideTitles: SideTitles(showTitles: false),
+                ),
+              ),
+
+              borderData: FlBorderData(show: false),
+
+              lineBarsData: [
+                LineChartBarData(
+                  spots: animatedSpots,
+                  isCurved: true,
+                  curveSmoothness: 0.25,
+                  preventCurveOverShooting: true,
+
+                  gradient: LinearGradient(
+                    colors: [
+                      Colors.blue.shade600,
+                      Colors.blue.shade400,
                     ],
                   ),
-                );
-              },
+
+                  barWidth: 3,
+
+                  dotData: FlDotData(show: 
+                    animation.value > 0.99,
+                  ),
+
+                  belowBarData: BarAreaData(
+                    show: true,
+                    gradient: LinearGradient(
+                      colors: [
+                        Colors.blue.withOpacity(0.3),
+                        Colors.transparent,
+                      ],
+                    ),
+                  ),
+                ),
+              ],
             ),
-          ),
-        ],
+            duration: Duration.zero,
+          );
+        },
       ),
     );
   }
