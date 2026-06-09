188b9c2706b5c8e64bd15386d94d22a758f72f77
skshreek2 <103485010+skshreek2@users.noreply.github.com>
2026-06-09 13:06:06 +0530

    Update newfile.dart

--- newfile.dart
+++ newfile.dart
@@ -1,49 +1,56 @@
+import 'package:intl/intl.dart';
+
 class DateRange {
   final String fromDate;
   final String toDate;
 
-  DateRange({
+  const DateRange({
     required this.fromDate,
     required this.toDate,
   });
 }
 
-import 'package:intl/intl.dart';
+class DateRangeHelper {
+  static final DateFormat _formatter = DateFormat('yyyy-MM-dd');
 
-DateRange _getDateRange(String filter) {
-  final now = DateTime.now();
-  final formatter = DateFormat('yyyy-MM-dd');
+  static DateRange getDateRange(String filter) {
+    final now = DateTime.now();
 
-  switch (filter) {
-    case 'Yesterday':
-      final yesterday = now.subtract(const Duration(days: 1));
-      return DateRange(
-        fromDate: formatter.format(yesterday),
-        toDate: formatter.format(yesterday),
-      );
+    switch (filter) {
+      case 'Yesterday':
+        final yesterday = now.subtract(const Duration(days: 1));
+        return DateRange(
+          fromDate: _formatter.format(yesterday),
+          toDate: _formatter.format(yesterday),
+        );
 
-    case 'Today':
-      return DateRange(
-        fromDate: formatter.format(now),
-        toDate: formatter.format(now),
-      );
+      case 'Today':
+        return DateRange(
+          fromDate: _formatter.format(now),
+          toDate: _formatter.format(now),
+        );
 
-    case 'Weekly': // Last 7 Days
-      return DateRange(
-        fromDate: formatter.format(now.subtract(const Duration(days: 6))),
-        toDate: formatter.format(now),
-      );
+      case 'Weekly': // Last 7 Days
+        return DateRange(
+          fromDate: _formatter.format(
+            now.subtract(const Duration(days: 6)),
+          ),
+          toDate: _formatter.format(now),
+        );
 
-    case 'Monthly': // Last 30 Days
-      return DateRange(
-        fromDate: formatter.format(now.subtract(const Duration(days: 29))),
-        toDate: formatter.format(now),
-      );
+      case 'Monthly': // Last 30 Days
+        return DateRange(
+          fromDate: _formatter.format(
+            now.subtract(const Duration(days: 29)),
+          ),
+          toDate: _formatter.format(now),
+        );
 
-    default:
-      return DateRange(
-        fromDate: formatter.format(now),
-        toDate: formatter.format(now),
-      );
+      default:
+        return DateRange(
+          fromDate: _formatter.format(now),
+          toDate: _formatter.format(now),
+        );
+    }
   }
 }
